ed4f160e27e0c34eb42338408776ea4eb8a28057
FranciscoMont-UC3M <73016833+FranciscoMont-UC3M@users.noreply.github.com>
2022-12-09 14:26:30 +0100

    Updated students01 to be a tutorial example, added some constraints to the variables. To test.

--- parte-1/CSP-calls.sh
+++ parte-1/CSP-calls.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Copy and paste to execute after doing a "cd parte-1" to access to folder appropriately
 python parte1/CSPBusSeats.py students01
+python parte1/CSPBusSeats.py students02
--- parte-1/CSPBusSeats.py
+++ parte-1/CSPBusSeats.py
@@ -27,6 +27,14 @@ array_bus = ["blue", "blue", "blue", "blue",
              "white", "white", "white", "white",
              "white", "white", "white", "white"]
 
+
+# The array of students stores all students as read from the file, in order.
+# Student with id number 1 will be in position 0, and so on, id number i will be in position i-1 of the array.
+'''This could be problematic if we didn't get id's in order or with jumps of number, 
+    in which case we would need to store them in a list and create a find(id) function to search them. 
+    For the sake of simplicity we assume the data is provided ordered and i-1 position is element with id i, but
+    we could have made such version too, at higher computational time'''
+
 array_students=[]
 with open("CSP-tests/" + students_path + ".txt") as textFile:
     for line in textFile:
@@ -41,15 +49,22 @@ print(matrix_students)
 
 problem = Problem()
 
+# Domains with proper numbers for the seats (i+1 because arrays start on zero) depending on colour
 domainWhite = []
 domainBlue = []
 for i in range(len(array_bus)):
     if array_bus[i] == "blue":
-        domainBlue.append(i)
+        domainBlue.append(i+1)
     if array_bus[i] == "white":
-        domainWhite.append(i)
+        domainWhite.append(i+1)
+# print(domainBlue)
+# print(domainWhite)
 
-# Add student variables (student_id's) and their domains to the solution, considering restricted mobility for domain
+arrayVariables = []
+for i in range(len(matrix_students)):
+    arrayVariables.append(matrix_students[i][0])
+
+'''Add variables (student_id's) and their domains to the problem, domain array is set considering restricted mobility'''
 for i in range(len(matrix_students)):
     # print("element in row "+str(i)+" has restriction variable "+str(matrix_students[i][3]))
     if matrix_students[i][3] == 'X':
@@ -57,3 +72,43 @@ for i in range(len(matrix_students)):
     elif matrix_students[i][3] == 'R':
         problem.addVariable(matrix_students[i][0], domainBlue)
 
+
+'''Constraint checks that the seats assigned from domain are a single seat and not assigned repeatedly'''
+def uniqueSingleSeat(*args):
+    for i in range(len(args)):
+        for j in range(i+1, len(args)):
+            if i != j and args[i] == args[j]:  # if different variables with same domain (sharing seat), not valid
+                return False
+        if len(args[i] != 1):  # if using none/several seats, not valid
+            return False
+    return True
+
+problem.addConstraint(uniqueSingleSeat, arrayVariables)
+
+'''Constraint for restricted mobility is valid only if the seat assigned is blue and adjacent one is also free'''
+def ifRestrictedThenSeatIsFreeBlue(*args):
+    for i in range(len(args)):
+        #If i is restricted student, then compare to all other ones (i!=j) and see if anyone is in adjacent blue
+        for j in range(len(args)):
+            if i != j and args[i] == args[j]:  # if different variables with same domain (sharing seat), not valid
+                return False
+        if len(args[i] != 1):  # if using none/several seats, not valid
+            return False
+    return False
+
+def seatIsFreeBlue(a):
+    if array_bus[a-1] == "blue":
+        if a % 2 == 0 and seatIsUsed(a-1):
+            return True
+    return False
+
+def seatIsUsed(b):
+    for j in range(len(arrayVariables)):
+        if matrix_students[j] == b:
+            return True
+    return False
+
+'''
+if 3 in domainBlue:
+    print("3 in domainBlue")
+'''
