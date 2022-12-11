6a1cc26d218981bc16876a75a97cde4352198e7e
FranciscoMont-UC3M <73016833+FranciscoMont-UC3M@users.noreply.github.com>
2022-12-11 14:27:25 +0100

    First 5 constraints are complete, but the constraints fail for more than 2 students. Put some constraints as comments and try chnaging functions aybe with new tests until bugs fixed?

--- parte-1/CSP-calls.sh
+++ parte-1/CSP-calls.sh
@@ -4,9 +4,9 @@ python CSPBusSeats.py students01 # test with a single restricted (constraints wo
 python CSPBusSeats.py students02 # test with two restricted and one troublesome (constraints work up to 5, and 6 remain)
 python CSPBusSeats.py students21 # test with two restricted and one troublesome brothers (constraints work up to 5, and 6 remain)
 python CSPBusSeats.py students03 # test should fail - 3 restricted troublesomes in the back where only 2 fit
-                                 # (constraints for restricted/trouble NOT WORKING, they get seated together)
+                                 # (constraints for restricted/trouble NOT WORKING - it gives solutions sitting them together)
 python CSPBusSeats.py students04 # test with four restricted and troublesome students (2 first year, 2 second year)
-
+                                 # (constraints for restricted and troublesome fail - 17,19,1,2 as solution)
 python CSPBusSeats.py students06 # test with six restricted and troublesome students
 python CSPBusSeats.py students07 # test with six restricted, troublesome students
 python CSPBusSeats.py students08 # test with eight students, copied from the statement
