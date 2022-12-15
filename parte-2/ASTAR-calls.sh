e197ccb7fc10462d0f5d6cd15f02a96f494acffa
100455376 <105248534+100455376@users.noreply.github.com>
2022-12-15 22:43:36 +0100

    Added test and call

--- parte-2/ASTAR-calls.sh
+++ parte-2/ASTAR-calls.sh
@@ -2,6 +2,8 @@
 # Copy and paste to execute after doing a "cd parte-2" to access to folder appropriately
 python ASTARBusQ.py students02 1
 # Test with 2 reduced mobility students, must fail, used to check queue_validity function successfully
+python ASTARBusQ.py students04 1
+# Test with 3 students (one troublesome, no reduced mobility)
 python ASTARBusQ.py students23 1
 # Test with 2 students (one troublesome), must work as 1CX and 2XX in whatever order, with cost = 3
 python ASTARBusQ.py students24 1
